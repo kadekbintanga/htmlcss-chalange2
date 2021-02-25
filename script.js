a37fdaee9d9482373ca1b7317ec70e4b11f9ed7f
Bintang <kadekbintanga@gmail.com>
2021-02-25 19:26:38 +0800

    Implementation Angular JS and develop UI design

---
+++ script.js
@@ -0,0 +1,28 @@
+var app = angular.module('myApp',[]);
+
+app.controller('myCtrl', function($scope){
+    $scope.data = data;
+});
+
+var data = [{
+    id:0,
+    title: 'BASIC',
+    price: './source/image/4 dolar.png',
+    benefit1: '10 Projects',
+    benefit2: '10 Pages',
+    benefit3: '100 Mb Disk Space'
+},{
+    id:1,
+    title: 'STANDARD',
+    price: './source/image/8 dolar.png',
+    benefit1: '20 Projects',
+    benefit2: '20 Pages',
+    benefit3: '200 Mb Disk Space'
+},{
+    id:2,
+    title: 'ADVANCED',
+    price: './source/image/12 dolar.png',
+    benefit1: '40 Projects',
+    benefit2: '40 Pages',
+    benefit3: '500 Mb Disk Space'
+}];
